dd3ccbc31460e2b3f89bbc21834335aab5450531
Sara <smsj318@gmail.com>
2025-02-17 19:48:05 +0000

    Salvado

---
+++ 2. estrutura condicional/1. estrutura_simples copy.py
@@ -0,0 +1,11 @@
+import os
+os.system("clear")
+
+idade = 10
+
+if idade < 18:
+    print("Acesso Negado.")
+
+print("== FIM ==")
+
+
---
+++ 2. estrutura condicional/3.  estrutura_aninhada.py
@@ -0,0 +1,16 @@
+import os
+os.system("clear")
+
+idade = 16
+
+if idade < 12:
+    print("Acesso Negado.")
+elif idade < 18:
+    print("Somente com permissão dos pais.")
+else:
+    print("Acesso Permitido.")
+
+    
+print("== FIM ==")
+
+
