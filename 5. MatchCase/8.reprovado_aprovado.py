5e3cde77344c2cd39c89ab89d4d077c530c057be
Sara <smsj318@gmail.com>
2025-03-10 19:54:57 +0000

    exercício

---
+++ 5. MatchCase/8.reprovado_aprovado.py
@@ -0,0 +1,9 @@
+import os
+os.system ("clear")
+
+aluno = input('Informe seu nome: ')
+nota_um = float(input('Informe sua primeira nota: '))
+nota_dois = float(input('Informe sua segunda nota: '))
+
+media = (nota_um + nota_dois) / 2
+
