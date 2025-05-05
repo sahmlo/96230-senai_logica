869ee6530dcd8de7a9cb04a6b767f6ee46400587
Sara <smsj318@gmail.com>
2025-05-05 19:52:49 +0000

    Classes

--- 10. Classes/11. Informacoes_pessoais.py
+++ 10. Classes/11. Informacoes_pessoais.py
@@ -3,7 +3,7 @@ os.system("clear")
 from dataclasses import dataclass
 
 @dataclass
-class Informacôes_pessoas:
+class Informacoes_funcionarios:
     nome: str
     data_nascimento: str
     rg: str
@@ -11,26 +11,26 @@ class Informacôes_pessoas:
     def exibir_dados(self):
         print(f"Nome: {self.nome} \nData de Nascimento: {self.data_nascimento} \nRG: {self.rg} \nCPF: {self.cpf}\n")
 
-imformacoes_pessoas = []
+lista_de_fucionarios = []
 PESSOA = 5
 
 for i in range(PESSOA):
-    funcionarios= Informacôes_pessoas(
+    funcionarios= Informacoes_funcionarios(
                                 nome=input("Digite o seu nome: "),
                                 data_nascimento=input("Digite sua data de nascimento: "),         
                                 rg= input("Digite seu RG: "),
                                 cpf= input("Digite seu CPF: ")
                                 )
-    imformacoes_pessoas.append(funcionarios)
+    lista_de_fucionarios.append(funcionarios)
     os.system("clear")
 
-nome_arquivo = ("Dados_funcionarios,txt")
+nome_arquivo = ("Dados_funcionarios.txt")
 with open(nome_arquivo, "a") as pessoas_funcionarios: 
-    for funcionarios in imformacoes_pessoas:
+    for funcionarios in lista_de_fucionarios:
         pessoas_funcionarios.write(f"{funcionarios.nome}, {funcionarios.data_nascimento}, {funcionarios.rg}, {funcionarios.cpf}\n")
 print("DADOS SALVOS COM SUCESSO")
 os.system("clear")
 print("===Dados dos Funcionários===")
 
-for funcionarios in imformacoes_pessoas:
+for funcionarios in lista_de_fucionarios:
     funcionarios.exibir_dados()
