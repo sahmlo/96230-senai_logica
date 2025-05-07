2da6e79021cfa5cbe22a91c887974a59edafcc6d
Sara <smsj318@gmail.com>
2025-05-07 19:37:50 +0000

    Classes

--- 10. Classes/13. Informacao_do_proletariado.py
+++ 10. Classes/13. Informacao_do_proletariado.py
@@ -11,19 +11,19 @@ class Funcionario:
 
 def exibir_dados(self):
     print(f"Nome: {self.nome} \nData de Nascimento: {self.data_nascimento} \nRG: {self.rg} \nCPF: {self.cpf}\n")
+
 informacoes_funcionarios = []
-PESSOA = 1
+PESSOA = 5
 
 for i in range(PESSOA):
     funcionario= Funcionario(
-                            nome=input("Digite o seu nome: "),
-                            data_nascimento=input("Digite sua data de nascimento: "),         
-                            rg= input("Digite seu RG: "),
-                            cpf= input("Digite seu CPF: ")
+                            nome = input("Digite o seu nome: "),
+                            data_nascimento = input("Digite sua data de nascimento: "),         
+                            rg = input("Digite seu RG: "),
+                            cpf = input("Digite seu CPF: ")
                             )
     informacoes_funcionarios.append(funcionario)
     os.system("clear")                         
-
 
 arquivo_nome = ("Funcionarios.txt")
 with open (arquivo_nome, "a") as funcionarios_pessoas:
@@ -31,10 +31,8 @@ with open (arquivo_nome, "a") as funcionarios_pessoas:
         funcionarios_pessoas.write(f"{funcionarios.nome}, {funcionarios.data_nascimento}, {funcionarios.rg}, {funcionarios.cpf}\n")
 print("DADOS SALVOS COM SUCESSO")
 os.system("clear")
-print("===Dados dos Funcionários===")
-
-
-print("EXIBINDO ARQUIVO")
+print("=== Dados dos Funcionários ===\n")
+print("EXIBINDO ARQUIVO\n")
 
 try:
     with open(arquivo_nome, "r", encoding="utf-8") as arquivo:
