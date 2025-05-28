cc9c36412199e42b88ecef514bd4bd5876111f41
Aluno Dendezeiros <aluno.den@senaibahia.com.br>
2025-05-28 16:21:12 -0300

    JAVASCRIPT

--- 1. Introdução ao JavaScript/16_vetores_com_objetos.js
+++ 1. Introdução ao JavaScript/16_vetores_com_objetos.js
@@ -1,4 +1,4 @@
-// Objetos
+// Objetos 
 const usuarios = [
     {nome: 'Ana', idade: 25},
     {nome: 'Marta', idade: 32},
@@ -31,6 +31,5 @@ const usuarioEncontrado = usuarios.find(usuario => usuario.nome === 'Marta')
 console.log(`Nome: ${usuarioEncontrado.nome}, idade: ${usuarioEncontrado.idade}`)
 
 console.log('\nMostra a soma de todas as idades.')
-const somaIdades = usuarios.reduce((total, usuario) => total + usuario,idade, 0)
+const somaIdades = usuarios.reduce((total, usuario) => total + usuario.idade, 0)
 console.log(`Total: ${somaIdades}`)
-
